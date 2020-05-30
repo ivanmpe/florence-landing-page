2b2862f6abc8d35ded37d22b469a361eab9b5480
Ivan Alves <ivan_alves94@hotmail.com>
2020-05-29 22:09:13 -0300

    wip: history

--- src/app/attraction/attraction.component.ts
+++ src/app/attraction/attraction.component.ts
@@ -1,3 +1,4 @@
+import { Attraction } from './../models/attration.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -9,7 +10,24 @@ export class AttractionComponent implements OnInit {
 
   constructor() { }
 
+  public attractions : Attraction[] = [];
+  public attraction : Attraction;
+
+
   ngOnInit() {
+    this.attractions.push(new Attraction({ Title : "Piazza",   Text: "12", Img: "assets/img/attraction/piazza.jpg"}))
+    this.attractions.push(new Attraction({ Title : "Cathedral",   Text: "12", Img: "assets/img/attraction/cathedral.jpg"}))
+   /*  this.attractions.push(new Attraction({ Title : "piaza",   Text: "12", Img: "assets/img/attraction/3.jpg"}))
+    this.attractions.push(new Attraction({ Title : "piaza",   Text: "12", Img: "assets/img/attraction/4.jpg"}))
+    this.attractions.push(new Attraction({ Title : "piaza",   Text: "12", Img: "assets/img/attraction/5.jpg"})) */
+    console.log(this.attractions);
+    this.attraction = new Attraction();
+
+  }
+
+  active(attraction: Attraction){
+      this.attraction.Title = attraction.Title;
+      this.attraction.Text = attraction.Text;
   }
 
 }
